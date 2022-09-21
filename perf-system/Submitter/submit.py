33569e2a8917e023460c9bedb9e2c85e660c4189
Fotis Kounelis <fotisss17@gmail.com>
2022-09-21 17:19:45 +0100

    add analysis component calculating troughput

--- perf-system/Submitter/submit.py
+++ perf-system/Submitter/submit.py
@@ -3,13 +3,13 @@
 With cp-ing workspace in CCF build folder you should be able to run submitter here
 """
 
-import re
 import aiohttp
 import asyncio
 import ssl
 import pandas as pd
 import fastparquet as fp
 import sys
+import time
 
 df = pd.read_parquet('../Generator/requests.parquet', engine='fastparquet')
 df_sends = pd.DataFrame(columns=["messageID", "sendTime"])
@@ -20,7 +20,6 @@ async def read():
     print(certificates[0])
     sslcontext = ssl.create_default_context(cafile=certificates[0])
     sslcontext.load_cert_chain(certificates[1], certificates[2])
-    import time
 
     start = time.time()
     for i, r in df.iloc[1:].iterrows():
@@ -36,15 +35,13 @@ async def read():
                 df_sends.loc[i-1] = [i, time.time()]
                 async with session.get('https://127.0.0.1:8000' + req[1], ssl=sslcontext) as resp:
                     t = await resp.text()
-                    df_receives.loc[i-1] = [i, time.time(), t]
-
+                    df_receives.loc[i-1] = [i, time.time(), resp.url.scheme + str(resp.version.major) + str(resp.version.minor) +\
+                                            "$" + str(resp.status) + "$" + resp.reason + "$" + str(resp.raw_headers)]
 
     end = time.time()
     print(end - start)
-    # print(df_sends)
-    # print(df_receives)
+    
     fp.write("./sends.parquet", df_sends)
     fp.write("./receives.parquet", df_receives)
-    sys.exit(2)
 
 asyncio.run(read())
