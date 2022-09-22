284dfeac7155cff6b3130e04cfbde77567419da7
Fotis Kounelis <fotisss17@gmail.com>
2022-09-22 10:34:24 +0100

    dropped the yaml support

--- perf-system/Analyzer/analysis.py
+++ perf-system/Analyzer/analysis.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import time
 
 df_sends = pd.read_parquet('../Submitter/sends.parquet', engine='fastparquet')
 df_receives = pd.read_parquet('../Submitter/receives.parquet', engine='fastparquet')
@@ -10,7 +9,7 @@ time_spent_sum = 0
 
 # the number of rows in both dfs is the same
 for i in range(len(df_sends.index)):
-    # assert df_sends[i]["messageID"] == df_receives[i]["messageID"], "the IDs do not match"
+    assert df_sends.iloc[i]["messageID"] == df_receives.iloc[i]["messageID"], "the IDs do not match"
     req_resp = df_receives.iloc[i]["rawResponse"].split("$")
     if req_resp[2] == "OK":
         successfull_reqs += 1
--- perf-system/Generator/generator.py
+++ perf-system/Generator/generator.py
@@ -2,45 +2,41 @@ import sys
 import argparse
 import pandas as pd
 import fastparquet as fp
-import yaml
 
 REQUEST_CONTENT_TYPE = "Content-Type: application/json"
 REQUEST_LENGTH_TEXT = "Content-Length: "
 PARQUET_FILE_NAME = "requests.parquet"
 
-class Generator():
-    def __init__(self):
-        self.df = pd.DataFrame(columns=["messageID", "request"])
 
-    def create_df(self, req_path, req_type, req_verb, req_iters, conf_file):
-        """
-        Creates a dataframe with the data 
-        required for the requests
-        """
-        # entering the private file paths as metadata in the start of parquet
+def create_df(df, req_path, req_type, req_verb, req_iters):
+    """
+    Creates a dataframe with the data 
+    required for the requests
+    """
+    # entering the private file paths as metadata in the start of parquet
 
-        if req_verb == "POST":
+    if req_verb == "POST":
 
-            for iter in range(req_iters):
-                request_message = '{"id": ' + str(iter) + ', "msg": "Send message with id ' + str(iter) + '"}'
+        for iter in range(req_iters):
+            request_message = '{"id": ' + str(iter) + ', "msg": "Send message with id ' + str(iter) + '"}'
 
-                self.df.loc[iter] = [str(iter), req_verb + "$" + req_path + "$" + req_type + "$" \
-                                    + REQUEST_CONTENT_TYPE + "$" + REQUEST_LENGTH_TEXT + \
-                                    str(len(request_message)) + "$" + request_message]
-        
-        elif req_verb == "GET":
-            for iter in range(req_iters):
-                self.df.loc[iter] = [str(iter), req_verb + "$" + req_path + "$" + req_type + "$" \
-                    + REQUEST_CONTENT_TYPE]
-        
+            df.loc[iter] = [str(iter), req_verb + "$" + req_path + "$" + req_type + "$" \
+                                + REQUEST_CONTENT_TYPE + "$" + REQUEST_LENGTH_TEXT + \
+                                str(len(request_message)) + "$" + request_message]
+    
+    elif req_verb == "GET":
+        for iter in range(req_iters):
+            df.loc[iter] = [str(iter), req_verb + "$" + req_path + "$" + req_type + "$" \
+                + REQUEST_CONTENT_TYPE]
+    
 
 
-    def create_parquet(self):
-        """
-        Takes the dataframe data and stores them 
-        in a parquet file in the current directory
-        """
-        fp.write(PARQUET_FILE_NAME, self.df)
+def create_parquet(df):
+    """
+    Takes the dataframe data and stores them 
+    in a parquet file in the current directory
+    """
+    fp.write(PARQUET_FILE_NAME, df)
         
 
 
@@ -55,21 +51,16 @@ def main(argv):
     parser.add_argument("-t", "--type", help="the type of the request", type=str)
     parser.add_argument("-vr", "--verb", help="the verb that specifies the action to the server")
     parser.add_argument("-r", "--rows", help="the number of request to be created", type=int)
-    parser.add_argument("-f", "--file", help="the path to a .yaml configuration file")
-    
 
     args = parser.parse_args()
 
-    yaml_input = yaml.safe_load(open(args.file or "./config.yaml"))
-
-    gen = Generator()
+    df = pd.DataFrame(columns=["messageID", "request"])
 
     #yaml has highest priority, over command line, over provided args
 
-    gen.create_df(yaml_input["configure"]["path"] or args.path or arg_path, args.type or arg_type,\
-                yaml_input["configure"]["verb"] or args.verb or arg_verb,\
-                yaml_input["configure"]["rows"] or args.rows or arg_iterations, args.file or "",)
-    gen.create_parquet()
+    create_df(df, args.path or arg_path, args.type or arg_type, args.verb or\
+                  arg_verb, args.rows or arg_iterations)
+    create_parquet(df)
 
 if __name__ == "__main__":
     main(sys.argv)
