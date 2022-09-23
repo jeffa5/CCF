5172d17842218e79214729a746d96c0db5c8e956
Fotis Kounelis <fotisss17@gmail.com>
2022-09-23 10:54:24 +0100

    add pass/fail requests on the analysis and prettify output

--- perf-system/analyzer/analysis.py
+++ perf-system/analyzer/analysis.py
@@ -5,11 +5,11 @@ Provide metrics based on the requests sent
 """
 
 import pandas as pd  # type: ignore
+from prettytable import PrettyTable
 
 df_sends = pd.read_parquet("../submitter/sends.parquet", engine="fastparquet")
 df_receives = pd.read_parquet("../submitter/receives.parquet", engine="fastparquet")
 
-print(str(len(df_sends.index)), " requests sent")
 SUCCESSFULL_REQUESTS = 0
 TIME_SPENT_SUM = 0
 
@@ -23,6 +23,25 @@ for i in range(len(df_sends.index)):
         SUCCESSFULL_REQUESTS += 1
     TIME_SPENT_SUM += df_receives.iloc[i]["receiveTime"] - df_sends.iloc[i]["sendTime"]
 
-print(round(TIME_SPENT_SUM, 1), "s total time for all requests")
-print(round(TIME_SPENT_SUM / SUCCESSFULL_REQUESTS, 3), "s per request on average")
-print(round(len(df_sends.index) / TIME_SPENT_SUM, 1), "req/s")
+PERCENTAGE_OF_SUCCESSFULL = SUCCESSFULL_REQUESTS / len(df_sends.index) * 100
+
+output_table = PrettyTable()
+output_table.field_names = [
+    "Total Requests",
+    "Total Time (s)",
+    "Pass (%)",
+    "Fail (%)",
+    "Throughput (req/s)",
+    "Avg (s/req)",
+]
+output_table.add_row(
+    [
+        len(df_sends.index),
+        round(TIME_SPENT_SUM, 1),
+        round(PERCENTAGE_OF_SUCCESSFULL, 1),
+        round(100 - PERCENTAGE_OF_SUCCESSFULL, 1),
+        round(len(df_sends.index) / TIME_SPENT_SUM, 1),
+        round(TIME_SPENT_SUM / len(df_sends.index), 3),
+    ]
+)
+print(output_table)
