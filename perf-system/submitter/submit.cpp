6e589d9ce13b106f84ae0654cf0088721cb03712
Fotis Kounelis <fotisss17@gmail.com>
2022-09-29 13:12:00 +0100

    add arguments in c++ file

--- perf-system/submitter/submit.cpp
+++ perf-system/submitter/submit.cpp
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the Apache 2.0 License.
 #include <vector>
 #include <chrono>
 #include <iostream>
@@ -17,6 +19,7 @@ std::vector<std::string> REQ_TYPE;
 std::vector<std::string> REQ_HEADER;
 std::vector<std::string> REQ_LENGTH;
 std::vector<std::string> REQ_DATA;
+
 
 const std::string WHITESPACE = "\", \n\r\t\f\v";
 
@@ -114,7 +117,7 @@ size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
     return nmemb;
 }
 
-std::vector<char> request(CURL *curl, std::vector<float> &times, int iter)
+std::vector<char> request(CURL *curl, std::vector<float> &times, int iter, std::vector<string> certificates)
 {
 
 	std::vector<char> response;
@@ -128,9 +131,11 @@ std::vector<char> request(CURL *curl, std::vector<float> &times, int iter)
 	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
 	curl_easy_setopt(curl, CURLOPT_WRITEDATA,  &response);
 
-    curl_easy_setopt(curl, CURLOPT_SSLCERT, "./user0_cert.pem");
-    curl_easy_setopt(curl, CURLOPT_SSLKEY, "./user0_privk.pem");
-    curl_easy_setopt(curl, CURLOPT_CAINFO, "./service_cert.pem");
+    if(certificates.size()>0) {
+        curl_easy_setopt(curl, CURLOPT_SSLCERT, certificates[0].c_str());
+        curl_easy_setopt(curl, CURLOPT_SSLKEY, certificates[1].c_str());
+        curl_easy_setopt(curl, CURLOPT_CAINFO, certificates[2].c_str());
+    }
 
     curl_easy_setopt(curl, CURLOPT_HEADER, 1L); // add in the response the headers
 
@@ -158,7 +163,16 @@ std::vector<char> request(CURL *curl, std::vector<float> &times, int iter)
     return response;
 }
 
-int main() {
+int main(int argc, char **argv) {
+
+    std::vector<string> certificates;
+
+    if(argc > 2) {
+        certificates.push_back(argv[1]);
+        certificates.push_back(argv[2]);
+        certificates.push_back(argv[3]);
+    }
+
     curl_global_init(CURL_GLOBAL_ALL);
     CURL *curl = curl_easy_init();
     std::vector<float> times;
@@ -166,7 +180,7 @@ int main() {
     readParquetFile();
 
     for (int iter = 0; iter < IDS.size(); ++iter) {
-        std::vector<char> response = request(curl, times, iter);
+        std::vector<char> response = request(curl, times, iter, certificates);
         for (auto& n : response)
             std::cout << n;
         cout<<endl;
