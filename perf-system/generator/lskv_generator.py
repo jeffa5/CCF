b12eb891be64c4a960dd2d405447aa06952a6471
Andrew Jeffery <dev@jeffas.io>
2022-10-21 12:33:32 +0000

    Update lskv generator

--- perf-system/generator/lskv_generator.py
+++ perf-system/generator/lskv_generator.py
@@ -3,43 +3,41 @@
 
 import pandas as pd  # type: ignore
 from generator import create_parquet, create_post
+from loguru import logger
 
 
-def make_requests(http_ver: int, host="127.0.0.1:8000"):
-    df = pd.DataFrame(columns=["messageID", "request"])
+def make_requests(host="127.0.0.1:8000"):
+    logger.info("get new dataframe")
+    df = []
 
-    if http_ver == 1:
-        http = "HTTP/1.1"
-    else:
-        http = "HTTP/2"
+    # not used in submitter
+    http = "HTTP/0"
 
-    for _ in range(10):
-        path = "/v3/kv/range"
-        data = '{"key":"aGVsbG8="}'
-        create_post(df, host, path, http, data)
-
+    logger.info("add range requests")
     for _ in range(2000):
         path = "/v3/kv/range"
         data = '{"key":"aGVsbG8="}'
         create_post(df, host, path, http, data)
 
+    logger.info("add put requests")
     for _ in range(1000):
         path = "/v3/kv/put"
         data = '{"key":"aGVsbG8=","value":"d29ybGQ="}'
         create_post(df, host, path, http, data)
 
+    logger.info("add range requests")
     for _ in range(10000):
         path = "/v3/kv/range"
         data = '{"key":"aGVsbG8="}'
         create_post(df, host, path, http, data)
 
+    logger.info("add delete requests")
     for _ in range(1000):
         path = "/v3/kv/delete_range"
         data = '{"key":"aGVsbG8="}'
         create_post(df, host, path, http, data)
 
-    create_parquet(df, f"lskv-{http_ver}.parquet")
+    create_parquet(df, f"lskv.parquet")
 
 
-make_requests(1)
-make_requests(2)
+make_requests()
